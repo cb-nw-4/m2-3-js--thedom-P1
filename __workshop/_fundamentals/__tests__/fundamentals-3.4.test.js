87b32ff85c6ba218c0113d016930e70755d03bdf
Mallory Belgica <mallorybelgica@gmail.com>
2020-09-26 17:17:37 -0400

    Completed fundamentals 3.4

--- __workshop/_fundamentals/__tests__/fundamentals-3.4.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.4.test.js
@@ -4,6 +4,10 @@ const sum = require("../fundamentals-3.4");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 4", () => {
   expect(sum([3, 4, 5])).toBe(12);
+  expect(sum([8, 20, 5, 10, 50])).toBe(93);
+  expect(sum([20, "fifteen", 15, 13])).toBe(undefined);
+  expect(sum([10, 10, 450])).toBe(470);
+  expect(sum(["this", "is", "a", "test"])).toBe(undefined);
   // add more tests here...
 });
 
