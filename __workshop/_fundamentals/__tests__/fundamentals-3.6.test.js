7d1631cb72f68ef3c5a1c864d1b3b93a6791ad5e
Mallory Belgica <mallorybelgica@gmail.com>
2020-09-26 17:46:58 -0400

    Completed fundamentals 3.6

--- __workshop/_fundamentals/__tests__/fundamentals-3.6.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.6.test.js
@@ -5,6 +5,9 @@ const calculator = require("../fundamentals-3.6");
 test("Exercise 6", () => {
   expect(calculator(["add", 10, 15])).toBe(25);
   // add more tests here...
+  expect(calculator(["div", 200, 40])).toBe(5);
+  expect(calculator(["square", 150, 10])).toBe(undefined);
+  expect(calculator(["sub", 250, 50])).toBe(200);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
