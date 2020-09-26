a252fa298dbe0428ab99d5d027459f27150d1d53
Mallory Belgica <mallorybelgica@gmail.com>
2020-09-26 14:48:54 -0400

    Completed fundamentals 3.1

--- __workshop/_fundamentals/__tests__/fundamentals-3.1.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.1.test.js
@@ -6,6 +6,9 @@ test("Exercise 1", function () {
   expect(firstLetter("max")).toBe("m");
   expect(firstLetter("")).toBe(undefined);
   // add more tests here...
+  expect(firstLetter("This is the coolest day ever.")).toBe("T");
+  expect(firstLetter(["Once", "Upon", "A", "Time"])).toBe(undefined);
+  expect(firstLetter("Canada")).toBe("C")
 });
 
 // Run your test by typing yarn test fundamentals-3.1 in the terminal.
