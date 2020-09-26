69e1614a53815d97ec176f4c182ebc915509609d
Joel <joel.m.bernstein@gmail.com>
2020-09-26 15:18:57 -0400

    completed fundamentals-3.5

--- __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
@@ -5,6 +5,9 @@ const getLetterAtIndex = require("../fundamentals-3.5");
 test("Exercise 5", () => {
   expect(getLetterAtIndex("bacon", 2)).toBe("c");
   // add more tests here...
+  expect(getLetterAtIndex("Joel", 0)).toBe("J");
+  expect(getLetterAtIndex("Hello World", 5)).toBe(undefined);
+  expect(getLetterAtIndex([1, 2, 3], 0)).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
