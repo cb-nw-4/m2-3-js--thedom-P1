db2e581cd1180ec26cc9c49826d2f623986e5c24
Mallory Belgica <mallorybelgica@gmail.com>
2020-09-26 17:58:59 -0400

    Completed fundamentals 3.8

--- __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
@@ -5,6 +5,9 @@ const reverseIt = require("../fundamentals-3.8");
 test("Exercise 8", () => {
   expect(reverseIt("abcdefgh")).toBe("hgfedcba");
   // add more tests here...
+  expect(reverseIt("ti esrever dna ti pilF")).toBe("Flip it and reverse it")
+  expect(reverseIt("ti rehpiced ot evah uoy dna edoc terces a si sihT")).toBe("This is a secret code and you have to decipher it")
+  expect(reverseIt("SDRAWKCAB DNA SDRAWROF")).toBe("FORWARDS AND BACKWARDS")
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
