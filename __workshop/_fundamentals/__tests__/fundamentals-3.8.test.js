13a68dc191d7d9430d5bc9992ebd2df4d5772158
Sandra <sandrabess@hotmail.com>
2020-09-26 17:20:10 -0400

    ex 3.8 completed

--- __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
@@ -5,6 +5,10 @@ const reverseIt = require("../fundamentals-3.8");
 test("Exercise 8", () => {
   expect(reverseIt("abcdefgh")).toBe("hgfedcba");
   // add more tests here...
+  expect(reverseIt("how are you")).toBe("uoy era woh");
+  expect(reverseIt("h")).toBe("h");
+  expect(reverseIt("")).toBe("");
+  expect(reverseIt(123)).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
