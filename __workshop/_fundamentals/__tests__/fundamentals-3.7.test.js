28bb1dfe2d07238b7b2b739baed4cc451e5b9c54
Joel <joel.m.bernstein@gmail.com>
2020-09-26 19:41:08 -0400

    completed findamentals-3.7

--- __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
@@ -5,6 +5,9 @@ const repeat = require("../fundamentals-3.7");
 test("Exercise 7", () => {
   expect(repeat(["foo", 3])).toBe("foofoofoo");
   // add more tests here...
+  expect(repeat(["bacon", 4])).toBe("baconbaconbaconbacon");
+  expect(repeat([3, "foo"])).toBe(undefined);
+  expect(repeat(["foo", 0])).toBe(" ");
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
