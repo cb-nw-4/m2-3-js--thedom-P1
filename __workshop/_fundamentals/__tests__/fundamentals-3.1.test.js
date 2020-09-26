0a53a4ceb52d47ea0bf8db7b9c0b96b3cba02fed
Sandra <sandrabess@hotmail.com>
2020-09-26 12:05:11 -0400

    fund 3.1 completed

--- __workshop/_fundamentals/__tests__/fundamentals-3.1.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.1.test.js
@@ -6,6 +6,11 @@ test("Exercise 1", function () {
   expect(firstLetter("max")).toBe("m");
   expect(firstLetter("")).toBe(undefined);
   // add more tests here...
+  expect(firstLetter(123)).toBe(undefined);
+  expect(firstLetter(" max")).toBe(" ");
+  expect(firstLetter("Hell I am doing a test")).toBe("H");
+  expect(firstLetter("123")).toBe("1");
+
 });
 
 // Run your test by typing yarn test fundamentals-3.1 in the terminal.
