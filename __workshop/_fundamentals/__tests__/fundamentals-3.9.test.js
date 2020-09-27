15453300e373775655d994a0c5c02154baa2cf9d
Joel <joel.m.bernstein@gmail.com>
2020-09-26 21:12:01 -0400

    completed fundamentals-3.9

--- __workshop/_fundamentals/__tests__/fundamentals-3.9.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.9.test.js
@@ -5,6 +5,10 @@ const longestWord = require("../fundamentals-3.9");
 test("Exercise 9", () => {
   expect(longestWord("a bb ccc dddd e ff ggg")).toBe("dddd");
   // add more tests here...
+  expect(longestWord("the joke cars")).toBe("cars");
+  expect(longestWord("hello old friends")).toBe("friends");
+  expect(longestWord("aaa bbb ccc ddd")).toBe("ddd");
+
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
