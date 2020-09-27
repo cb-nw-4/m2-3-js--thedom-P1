91c0533b9af78b885e8159102380ea1e7c18b416
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-09-26 20:17:09 -0400

    Fundamentals-3.7 answered

--- __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
@@ -5,6 +5,9 @@ const repeat = require("../fundamentals-3.7");
 test("Exercise 7", () => {
   expect(repeat(["foo", 3])).toBe("foofoofoo");
   // add more tests here...
+  expect(repeat(["My cat's name is Tigris", 4])).toBe("My cat's name is TigrisMy cat's name is TigrisMy cat's name is TigrisMy cat's name is Tigris");
+  expect(repeat(["foo", "4"])).toBe(undefined);
+  expect(repeat(["ha", 10])).toBe("hahahahahahahahahaha");
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
