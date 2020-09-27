7304a345c49fd94ef862e2b8ad518a58d4bdedc4
Doug Wright <dw@nof49.ca>
2020-09-27 17:20:43 -0400

    Completed exercise 3.7

--- __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.7.test.js
@@ -4,7 +4,10 @@ const repeat = require("../fundamentals-3.7");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 7", () => {
   expect(repeat(["foo", 3])).toBe("foofoofoo");
-  // add more tests here...
+  expect(repeat(["bar", 5])).toBe("barbarbarbarbar");
+  expect(repeat([10, 3])).toBe(undefined);
+  expect(repeat(["", 3])).toBe("");
+  expect(repeat('foo, 3')).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
