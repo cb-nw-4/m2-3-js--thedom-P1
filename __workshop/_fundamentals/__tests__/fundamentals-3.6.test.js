17b3f69cf623692880eeda9f538016f50fd94d5d
Doug Wright <dw@nof49.ca>
2020-09-27 16:59:51 -0400

    Completed exercise 3.6

--- __workshop/_fundamentals/__tests__/fundamentals-3.6.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.6.test.js
@@ -4,7 +4,12 @@ const calculator = require("../fundamentals-3.6");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 6", () => {
   expect(calculator(["add", 10, 15])).toBe(25);
-  // add more tests here...
+  expect(calculator(["sub", 20, 10])).toBe(10);
+  expect(calculator(["mul", 5, 5])).toBe(25);
+  expect(calculator(["div", 10, 2])).toBe(5);
+  expect(calculator(["boo", 10, 15])).toBe(undefined);
+  expect(calculator(["add", 10])).toBe(undefined);
+  expect(calculator(["add", 'ten', 15])).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
