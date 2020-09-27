d28fdac34ad80a41e27e10e210afde759fd34594
Doug Wright <dw@nof49.ca>
2020-09-27 16:27:45 -0400

    Completed exercise 3.5

--- __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
@@ -4,7 +4,9 @@ const getLetterAtIndex = require("../fundamentals-3.5");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 5", () => {
   expect(getLetterAtIndex("bacon", 2)).toBe("c");
-  // add more tests here...
+  expect(getLetterAtIndex("", 2)).toBe(undefined);
+  expect(getLetterAtIndex(100, 2)).toBe(undefined);
+  expect(getLetterAtIndex("bacon", 10)).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
