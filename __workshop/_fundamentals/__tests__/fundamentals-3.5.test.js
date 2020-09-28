807679888a5463193100887504e1fbdc413d02a6
Doug Wright <dw@nof49.ca>
2020-09-28 19:28:49 -0400

    Added additional test to 3.5

--- __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.5.test.js
@@ -7,6 +7,7 @@ test("Exercise 5", () => {
   expect(getLetterAtIndex("", 2)).toBe(undefined);
   expect(getLetterAtIndex(100, 2)).toBe(undefined);
   expect(getLetterAtIndex("bacon", 10)).toBe(undefined);
+  expect(getLetterAtIndex(["bacon", 2])).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
