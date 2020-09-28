d37e818533112bbff1892009889700285bddff29
“Winnie <chanwinnie.wc@gmail.com>
2020-09-28 00:58:10 -0400

    Fundamentals-3.1 completed

--- __workshop/_fundamentals/fundamentals-3.1.js
+++ __workshop/_fundamentals/fundamentals-3.1.js
@@ -8,7 +8,12 @@
 // - If it's an empty string (''), return undefined.
 
 function firstLetter(str) {
-  // Your code here
+ 
+  if (str.length === 0) {
+  return undefined;
+  }
+  return str.charAt(0);
+  
 }
 
 // Part 2 - Test
