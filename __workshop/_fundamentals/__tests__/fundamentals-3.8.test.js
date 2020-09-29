eff1a40a68a23ffbba53b02f4268cf68bae323c0
“Winnie <chanwinnie.wc@gmail.com>
2020-09-29 01:02:04 -0400

    Fundamentals-3.8 completed

--- __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
+++ __workshop/_fundamentals/__tests__/fundamentals-3.8.test.js
@@ -5,6 +5,9 @@ const reverseIt = require("../fundamentals-3.8");
 test("Exercise 8", () => {
   expect(reverseIt("abcdefgh")).toBe("hgfedcba");
   // add more tests here...
+  expect(reverseIt("asoivel")).toBe("leviosa");
+  expect(reverseIt("!12345")).toBe("54321!");
+  expect(reverseIt("anna")).toBe("anna");
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
